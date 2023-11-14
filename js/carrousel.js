8b7032b26258bd0a7a7c6c0cac0940f936612adf
Matias <matias.virga@gmail.com>
2023-11-14 20:19:01 -0300

    Nuevo y ultim repositorio, proyecto terminado C54

---
+++ js/carrousel.js
@@ -0,0 +1,17 @@
+const grande = document.querySelector('.grande')
+const punto = document.querySelectorAll('.punto')
+
+punto.forEach( ( cadaPunto , i)=> {
+    punto[i].addEventListener('click',()=> {
+
+        let posicion = i
+        let operacion = posicion * -50
+
+        grande.style.transform = `translateX(${ operacion }%)`
+
+        punto.forEach( ( cadaPunto , i)=> {
+            punto[i].classList.remove('activo')
+            })
+        punto[i].classList.add('activo')
+    })
+})
